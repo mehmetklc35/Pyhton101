b4217f4c85df18a0b2e091ae262c1dcd25aa9f96
mehmet kılıç <mehmetkilic35@outlook.com>
2023-12-14 23:37:30 +0300

    Pythonda sayılar konusu işlendi. Pyhtonda_sayılar.py dosyası oluşturuldu.

---
+++ Pythonda_sayilar.py
@@ -0,0 +1,34 @@
+# # a = 2
+# # b = 3
+
+# # a_gecici = a
+# # a = b
+# # b = a_gecici
+
+# # print(f"a = {a}, b = {b}")
+
+# # a,b = b,a
+# # print(f"a = {a}, b = {b}")
+    
+# a = 10
+# a = a + 1
+# print(a)
+
+# a = 50
+# a *=2
+# print(a)
+
+# """
+# çoklu yorum satırı
+
+#     Author: mehmet kılıç
+    
+#     version: 1.
+    
+#     notes:bu kod dosyası 1000 satırdan oluşur.pdf dosyalarını convert etmeye yarar.
+
+# """
+
+
+
+    

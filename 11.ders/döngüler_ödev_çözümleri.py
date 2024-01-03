759d5f7e911aeaef59c7d51e41e265d605883593
mehmet kılıç <mehmetkilic35@outlook.com>
2024-01-03 20:04:21 +0300

    11.ders

---
+++ 11.ders/döngüler_ödev_çözümleri.py
@@ -0,0 +1,74 @@
+# # sayi = int(input("Lütfen sayıyı giriniz: "))
+
+# # toplam = 0
+# # i = 1
+# # while(i<sayi):
+# #     if(sayi%i == 0):
+# #         toplam += i
+# #     i += 1
+    
+# # if(toplam == sayi):
+# #     print("Bu sayi mükemmel sayıdır : ")
+# # else:
+# #     print("Bu sayi mükemmel sayı değildir : ",sayi)
+    
+        
+# while True:
+    
+#     Sayi = input("Lütfin sayınızı giriniz (Çıkmak için w ya basınız.) : ")
+#     basamak_sayisi = len(Sayi)
+#     sayi = int(sayi)
+#     basamak = 0
+#     toplam = 0
+    
+#     gecici_sayi = sayi
+    
+#     while(gecici_sayi>0):
+#         basamak = gecici_sayi % 10
+#         toplam += basamak ** basamak_sayisi
+#         gecici_sayi //= 10
+        
+#     if (sayi == toplam):
+#         print(f"Girilen sayı {sayi} dir. Ve bu bir armstrog sayısıdır.")
+#     else:
+#         print(f"Girilen sayı {sayi} dir. Ve bu bir armstrong sayısı değildir.")
+        
+        
+# for i in range(0,11):
+#     for j in range(0,11):
+#         print(f"{i} x {j} = {i*j}")
+    
+
+# toplam = 0
+# while True:
+#     sayi = input("Sayıyı giriniz (Çıkmak ve toplamı görebilmek için q'ya basabilirsiniz.")
+#     if (sayi == "q"):
+#         print(f"Girdiğiniz sayıların toplamı {toplam}' dir.")
+#         break
+#     toplam += int(sayi)
+
+
+
+
+
+# for i in range(1, 101):
+#     if i % 3 != 0:
+# #         continue
+# #     print(f"Sayımız: {i} ve bu sayı 3'e tam bölünür.")
+
+
+# liste = list()
+# for i in range(1,101):
+#     if i % 2 == 0:
+#         liste.append(i)
+# print(liste)
+
+# liste = [i for i in range(1,101) if i % 2 == 0]
+# print(liste)
+
+
+
+
+
+    
+    
